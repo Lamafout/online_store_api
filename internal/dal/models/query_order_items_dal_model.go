867f3ad92549f7e81d7cdbab30b8dc932d2769e9
Lamafout <dzordzvashington@gmail.com>
2025-10-04 02:22:44 +0300

    feat: models

---
+++ internal/dal/models/query_order_items_dal_model.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+)
+
+type QueryOrderItemsDalModel struct {
+	ID             int64     `db:"id"`
+	OrderID        int64     `db:"order_id"`
+	ProductID      int64     `db:"product_id"`
+	Quantity       int       `db:"quantity"`
+	ProductTitle   string    `db:"product_title"`
+	ProductURL     string    `db:"product_url"`
+	PriceCents     int64     `db:"price_cents"`
+	PriceCurrency  string    `db:"price_currency"`
+	CreatedAt      time.Time `db:"created_at"`
+	UpdatedAt      time.Time `db:"updated_at"`
+}
---
+++ internal/dal/models/v1_order_dal.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+)
+
+type V1OrderDal struct {
+	ID                int64     `db:"id"`
+	CustomerID        int64     `db:"customer_id"`
+	DeliveryAddress   string    `db:"delivery_address"`
+	TotalPriceCents   int64     `db:"total_price_cents"`
+	TotalPriceCurrency string   `db:"total_price_currency"`
+	CreatedAt         time.Time `db:"created_at"`
+	UpdatedAt         time.Time `db:"updated_at"`
+}
